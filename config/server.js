dbb962f04e8553f3fea9192376319ed43001a7da
xiangdi su <xiangdi10@gmail.com>
2021-02-02 16:06:19 -0500

    Seo Components and Page

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'da350fff29eda765f3263f7930b35d19'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6eaf09f9-b82d-41e7-94e1-3ae76d2f82da'
+};
